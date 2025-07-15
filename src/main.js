87fdf04e910d2193c9faeae5cec38f80fab31350
Eddie <eddie123burciaga@gmail.com>
2025-07-14 20:10:09 -0600

    Initial_commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./css/sb-admin-2.min.css"; // or "./css/sb-admin-2.min.css"
+
+import $ from "jquery";
+import "bootstrap";
+import "jquery.easing";
+import DataTable from "datatables.net-bs5";
+import "datatables.net";
+import { Chart } from "chart.js";
+
+$(function () {
+  $("#dataTable").DataTable({
+    ajax: {
+      url: "https://trabajo-wbdc.onrender.com/getdata",
+      dataSrc: "", // Use "data" if your endpoint returns { data: [...] }
+    },
+    columns: [
+      { data: "id", title: "ID" },
+      { data: "nombre", title: "NAME" },
+      { data: "matricula", title: "MATRICULA" },
+      { data: "created_at", title: "CREATED_AT" },
+    ],
+    responsive: true,
+  });
+});
